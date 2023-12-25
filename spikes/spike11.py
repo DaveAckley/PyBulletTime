ddc750a814364e3860c91c6653d19ae9dfd8e028
Dave Ackley <ackley@livingcomputation.org>
2023-12-25 10:51:42 -0700

    end of module

--- spikes/spike11.py
+++ spikes/spike11.py
@@ -1,4 +1,9 @@
+import time
 import pybullet as p
 physicsClient = p.connect(p.GUI)
+#TO SUPPRESS SIDEBARS: p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+for i in range(1000):
+    p.stepSimulation()
+    time.sleep(1/60)
 p.disconnect()
 
