1066edb954762f866da719caead1922a2583926d
Dave Ackley <ackley@livingcomputation.org>
2023-12-27 01:49:36 -0700

    end of E. Joints

--- generate.py
+++ generate.py
@@ -2,24 +2,40 @@
 
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    length = 1
+    width = 1
+    height = 1
+    x = 0
+    y = 5
+    z = height/2
+
+    pyrosim.Send_Cube(name="Box10", pos=[x,y,z] , size=[length,width,height])
+    pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    length = 1
+    width = 1
+    height = 1
+    x = 0
+    y = 0
+    z = height/2
+
+    pyrosim.Send_Cube(name="Torso", pos=[x,y,z+1] , size=[length,width,height])
+
+    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" ,
+                        type = "revolute", position = [.5,0,1]) 
+    pyrosim.Send_Cube(name="FrontLeg", pos=[.5,0,-.5] , size=[length,width,height])
+
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" ,
+                        type = "revolute", position = [-.5,0,1]) 
+    pyrosim.Send_Cube(name="BackLeg", pos=[-.5,0,-.5] , size=[length,width,height])
+    
+    pyrosim.End()
+    
 
 
-length = 1
-width = 1
-height = 1
-
-x = 0
-y = 0
-z = height/2
-
-#pyrosim.Send_Cube(name="Box10", pos=[x,y,z] , size=[length,width,height])
-#pyrosim.Send_Cube(name="Box11", pos=[x,y,z] , size=[length,width,height])
-for x in range(4):
-    for y in range(4):
-        sz = [length,width,height]
-        for h in range(10):
-            pyrosim.Send_Cube(name="Box11", pos=[x,y,1.5*h] , size=sz)
-            sz = [.9*sz[0],.9*sz[1],.9*sz[2]]
-pyrosim.End()
-
+Create_World()
+Create_Robot()
