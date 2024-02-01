74e8c717906f8e275128dcf22790bd927e7a31ac
Dave Ackley <ackley@livingcomputation.org>
2024-02-01 13:08:48 -0700

    Render scripts

---
+++ scripts/newestSceneRender.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+use File::Basename;
+use Cwd 'abs_path';
+
+my $scriptDir = abs_path(dirname(__FILE__));
+my $tag = shift @ARGV;
+$tag and die "NO ARGS ALLOWED";
+my $datapath = "$scriptDir/data";
+
+opendir(H,$datapath) or die $!;
+my @datadirs = sort { $b cmp $a} grep { /^\d+-\d+$/ } readdir(H); 
+closedir(H) or die $!;
+
+scalar(@datadirs) or die "No data dirs in $datapath";
+my $newest = shift @datadirs;
+print("RENDERING $newest\n");
+exec('./renderScene.pl', $newest);
+
